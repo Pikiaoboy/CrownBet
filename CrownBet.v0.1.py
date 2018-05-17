7be12675d43473177b6aea14df91d5b6e06d6d0f
Peter Jones <Peter.Jones@genesisenergy.co.nz>
2018-05-17 16:41:42 +1200

    Update

--- CrownBet.v0.1.py
+++ CrownBet.v0.1.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 
 #Set website base url
@@ -14,10 +15,19 @@ base_url = "https://crownbet.com.au"
 sports_url = base_url+"/sports-betting"
 
 #Set output location
-base_file = "Results\\Crownbet\\"
+base_file = "C:\\Temp\\Results\\Crownbet\\"
 
 #load browser driver
 cb_driver = webdriver.Chrome()
+cb_driver.maximize_window()
+
+#WARNING
+#THIS DOES NOT WORK
+for i in range(5):
+    cb_driver.find_element_by_tag_name("html").send_keys(Keys.CONTROL, Keys.SUBTRACT)
+
+
+
 cb_driver.get(sports_url)
 
 #parse the main page
@@ -45,7 +55,7 @@ cb_driver.get(league_url)
 
 try:
     element_present = EC.presence_of_element_located((By.ID, 'smartbanner-wrapper'))
-    WebDriverWait(driver, delay).until(element_present)
+    WebDriverWait(cb_driver, delay).until(element_present)
 except TimeoutException:
     print("Timed out waiting for page to load")
 
@@ -57,9 +67,29 @@ match_url = base_url+matches_soccer[0].a.get('href')
 cb_driver.get(match_url)
 
 
+
+
+#match_soup.find_all("div", class_="drop-down-header clearfix closed ")
+cb_driver.get(match_url)
+#cb_driver.find_element_by_class_name("sports-event-wrap").find_elements_by_css_selector(".drop-down-header.clearfix")
+#cb_driver.find_element_by_xpath("//*[@id='middle-container']/div[3]/div[1]/div/div[1]").click()
+#cb_driver.find_element_by_xpath("//*[@id='middle-container']/div[3]").find_element_by_xpath("//div[1]/div/div[1]").click()
+
+#cb_driver.find_element_by_id("middle-container").click()
+
+elements = cb_driver.find_elements_by_xpath("//*[@data-stateless='true']")
+#elements = cb_driver.find_element_by_class_name("sports-event-wrap").find_elements_by_css_selector(".drop-down-header.clearfix")
+#elements = cb_driver.find_element_by_class_name("sports-event-wrap").find_elements_by_tag_name("div")
+length = len(elements)-1
+for y in range(length):
+    ele = length-y
+    elements[ele].click()
+
 #parses game page
 match_soup = BeautifulSoup(cb_driver.page_source, 'lxml')
 
+#gets bets and odds and writes to the file
+#currently only returns submenus that are open - todo: automate opening submenus
 odds_socc = match_soup.find_all("div", class_="match-type clearfix")
 match_name = match_soup.find("span", class_="item match-name").text.strip()
 #creates file name (game name)
@@ -67,8 +97,6 @@ file_name_socc =  (base_file +" " +match_name+".csv").replace("/","-")
 with open(file_name_socc, 'w') as f:
     f.write("Section,Selection,Odds\n")
 
-#gets bets and odds and writes to the file
-#currently only returns submenus that are open - todo: automate opening submenus
 
 for odd_socc in odds_socc:
     sec_soccer_name=odd_socc.find("div",class_="title multiple-events").span.text.strip()
@@ -79,5 +107,3 @@ for odd_socc in odds_socc:
             f.write(sec_soccer_name+","+lines_socc[x].text.strip()+","+bets_socc[x].text.strip()+"\n")
 
 #fin
-
-select = Select(cb_driver.find_element_by_class_name("cb-accordion-item__toggle-icon"))
